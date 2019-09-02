65bbcbd7be91e089490fa64e2b26e1dd31ab5915
ghitacornel <ghita.cornel@gmail.com>
2019-09-02 11:22:12 +0300

    better tests

--- src/test/java/model/TestInvokeConstructors.java
+++ src/test/java/model/TestInvokeConstructors.java
@@ -11,9 +11,13 @@ public class TestInvokeConstructors {
     public void testInvokeConstructor() throws Exception {
 
         Assert.assertEquals(1, SimpleClass.class.getConstructors().length);
+
+        // get constructor
         Constructor<SimpleClass> constructor = SimpleClass.class.getConstructor();
 
+        // invoke constructor
         SimpleClass instance = constructor.newInstance();
+
         Assert.assertNotNull(instance);
         Assert.assertNull(instance.constructorField);
 
@@ -23,9 +27,16 @@ public class TestInvokeConstructors {
     public void testInvokePrivateConstructor() throws Exception {
 
         Assert.assertEquals(2, SimpleClass.class.getDeclaredConstructors().length);
+
+        // get constructor
         Constructor<SimpleClass> constructor = SimpleClass.class.getDeclaredConstructor(String.class);
+
+        // make constructor accessible
         constructor.setAccessible(true);
+
+        // invoke constructor
         SimpleClass instance = constructor.newInstance("some constructor set value");
+
         Assert.assertNotNull(instance);
         Assert.assertEquals("some constructor set value", instance.constructorField);
 
